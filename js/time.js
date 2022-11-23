ebb821703958e1f4fd32643ec026c39eeebe219d
binayarimal <rimal.binaya@gmail.com>
2022-11-22 19:58:05 -0800

    almost done

--- js/time.js
+++ js/time.js
@@ -1,35 +1,45 @@
 
+/*
+  Create a web page that uses JavaScript to determine the current hour,
+  and presents a different image, custom message, and color based on each
+  of the following times of day:
 
+  Morning (5am to 12 noon)
+  Afternoon (12 noon to 6pm)
+  Evening (6pm to 11pm)
+  Night (11pm to 5am)
+*/
 
-myDate = new Date();
-myHour = myDate.getHours();
-myDay = myDate.getDay();
+let myDate = new Date();
+let myHour = myDate.getHours();
+let myDay = myDate.getDay();
 document.getElementById("current-time").innerHTML = myHour;
-let currDay; 
-let currGreeting;
-let currImage;
-let currColor;
+let currDay = ""; 
+let currGreeting ="";
+let currImage = "";
+let currColor = "";
+
 
 function currentTime(){
 if (myHour > 4 && myHour < 12){
     currGreeting = "Good Morning!";
-    currImage = "images/morning.jpg";
+    currImage = "morning.jpg";
     currColor = "yellow";
     
 } else if (myHour > 11 && myHour < 18){
     currGreeting = "Good Afternoon!";
-    currImage = "images/afternoon.jpg";
+    currImage = "afternoon.jpg";
     currColor = "green";
 
 } else if (myHour > 17 && myHour < 24){
     currGreeting = "Good Evening!";
-    currImage = "images/evening.jpg";
+    currImage = "evening.jpg";
     currColor = "orange";
     
 } else {
     currGreeting = "Good Night!";
-    currImage = "images/night.jpg";
-    currColor = "grey";
+    currImage = "night.jpg";
+    currColor = "blue";
 }
 
 }
@@ -60,11 +70,20 @@ function getCurrentDay(){
 getCurrentDay();
 currentTime();
 
-document.getElementById("currentYear").innerHTML = myDate.getFullYear();
+//Here we are placing the greeting on the page
 document.getElementById("greetings").innerHTML = currGreeting;
-document.getElementById("timePic").src = currImage;
-document.getElementById("greetings").style.backgroundColor = currColor;
+
+//Here we are changing the background color of the HTML tag
+document.querySelector("html").style.backgroundColor = currColor;
+
+// add images path to myPic 
+currImage = "images/" + currImage;
+
+// here we change the source
+document.getElementById("timePic").src =  currImage;
+
 document.getElementById("currentDay").innerHTML = currDay;
+document.getElementById("currentYear").innerHTML = myDate.getFullYear();
 
 
 
